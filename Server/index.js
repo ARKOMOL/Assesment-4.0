5ad6050b9cd5a1899e4d51831287c61fa261e048
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-06-11 22:20:47 +0600

    again

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const http = require("http")
 const {Server} = require("socket.io");
 
-//midlleware 
+//midlleware ====
 app.use(cors());
 app.use(express.json());
 
@@ -21,7 +21,7 @@ const io = new Server(server,{
 });
 
 
-//====================================
+//====================================///
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ra7na2o.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
